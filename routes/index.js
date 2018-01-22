3def2de559a361040d3a997e15e44ebf4a1de17d
myodehi <myodehi@gmail.com>
2018-01-22 00:19:25 +0000

    new

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,20 @@ router.get('/', (req, res) => {
   });
 });
 
+router.post('/submit-form', async (req, res) => {
+  var pg = require("pg");
+  var firstname = req.body.fname;
+  var lastname = req.body.lname;
+  var email = req.body.email;
+  var password = req.body.password[1];
+
+  const r = await req.pool.query("INSERT INTO users(firstname, lastname, email, password) VALUES($1, $2, $3, $4)", [firstname, lastname, email, password]);
+
+  console.log(r);
+  res.render('login', {
+    title: 'Crypto sale'
+  });
+});
+
+
 export default router;
